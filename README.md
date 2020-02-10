be79312c05cb9f7b399604e1848dcf09d52ba861
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2020-02-10 13:09:25 +0530

    021: First commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # IPTV
 IPTV application
+
+# Todo 
+
+1. onPage UI Changes
+2. Images from URL remove from local
+3. Make li from this url https://materializecss.com/collections.html (Avatar Content)
+4. createTab(Array);
+5. a
+```JavaScript
+[{
+    name: "Chapter 1",
+    category: "Tab 1",
+    logo: "",
+    description: "Lorem poreum",
+    url: ""
+},
+{
+    name: "Chapter 2",
+    category: "Tab 2",
+    logo: "",
+    description: "Lorem poreum",
+    url: ""
+}]
+
+var categories = getUnique(Array, "category"); // create those tabs and tab content using jQuery
+categories // loop through each and append the list 
+```
---
+++ www/js/script.js
@@ -0,0 +1,105 @@
+jQuery(function ($) {
+  playerDitails = JSON.parse(localStorage.getItem("playerData"));
+  // console.log("playerDitails_", playerDitails);
+  var $bodyEl = $('body'),
+    $sidedrawerEl = $('#sidedrawer');
+
+
+  $(playerDitails).each(function( i, n ) {
+    // console.log("playerDitails[i].value",n.name);
+    $(".leftMenuItem").append("<li><strong class=''>" + n.name + "</strong></li>");
+  });
+
+  $('.onPage').on('click', function () {
+    $(".playList").addClass("hidden")
+    $(".player_listing").addClass("hidden")
+    $('.player_form').removeClass('hidden');
+  });
+
+  $('.showPlayer').on('click', function () {
+    $(".playList").addClass("hidden")
+    $(".player_form").addClass("hidden")
+    $('.player_listing').removeClass('hidden');
+  });
+  $('.playerListDiv').on('click', function () {
+    $(".player_listing").addClass("hidden")
+    $(".player_form").addClass("hidden")
+    $('.playList').removeClass('hidden');
+  });
+
+
+
+  var playerDataArr = [];
+  $('.savebtn').on('click', function () {
+
+    if (localStorage.getItem("playerData") === null) {
+      console.log("localStorage is empty");
+
+      var id = Math.floor(Math.random() * 101);
+      var name = $('.playerName').val();
+      var url = $('.playerUrl').val();
+
+      var innerObj = {
+        "id": id,
+        "name": name,
+        "url": url
+      };
+      // playerData.push({"id":id, "name": name, "url": url })
+      console.log(innerObj);
+      playerDataArr.push(innerObj)
+
+      $('.playerName').val('');
+      $('.playerUrl').val('');
+      // console.log("playerData________", playerDataArr);
+      localStorage.setItem("playerData", JSON.stringify(playerDataArr));
+    } else {
+
+      var existing = JSON.parse(localStorage.getItem("playerData"));
+
+      var id = Math.floor(Math.random() * 101);
+      var name = $('.playerName').val();
+      var url = $('.playerUrl').val();
+
+      var innerObj = {
+        "id": id,
+        "name": name,
+        "url": url
+      };
+      console.log("innerObj", innerObj);
+
+      playerDataArr.push(innerObj)
+      for (var i = 0; i < existing.length; i++) {
+        // console.log("old data", existing[i]);
+        playerDataArr.push(existing[i]);
+        // console.log("existing[i]", existing[i]);
+      }
+      console.log("new data playerDataArr", innerObj.name);
+      // existing = existing ? existing.split(',') : [];
+      // var id = Math.floor(Math.random() * 101);
+      // var name = $('.playerName').val();
+      // var url = $('.playerUrl').val();
+      // var playerData_update = [];
+      // var innerObj = {
+      //   "id": id,
+      //   "name": name,
+      //   "url": url
+      // };
+      // playerData_update.push(innerObj)
+      // playerDitails.push(playerData_update);
+      // var existing = localStorage.getItem('myFavoriteSandwich');
+
+      // Otherwise, convert the localStorage string to an array
+      // existing = existing ? existing.split(',') : [];
+
+      // existing.push(playerData_update);
+
+      // localStorage.setItem('playerData', existing.toString());
+
+      // localStorage.setItem(name, existing.toString());
+      $('.playerName').val('');
+      $('.playerUrl').val('');
+    }
+    localStorage.setItem("playerData", JSON.stringify(playerDataArr));
+    location.reload();
+  })
+});
