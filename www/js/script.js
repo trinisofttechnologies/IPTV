860d1e3338689b310b3dd2e1a6d5a64d8ceee9f3
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2020-02-10 19:29:40 +0530

    021: add change in listing and add file

--- www/js/script.js
+++ www/js/script.js
@@ -1,19 +1,22 @@
 jQuery(function ($) {
   playerDitails = JSON.parse(localStorage.getItem("playerData"));
-  // console.log("playerDitails_", playerDitails);
+  console.log("playerDitails_", playerDitails);
   var $bodyEl = $('body'),
     $sidedrawerEl = $('#sidedrawer');
 
-
-  $(playerDitails).each(function( i, n ) {
-    // console.log("playerDitails[i].value",n.name);
+  $(playerDitails).each(function (i, n) {
     $(".leftMenuItem").append("<li><strong class=''>" + n.name + "</strong></li>");
   });
 
-  $('.onPage').on('click', function () {
-    // $(".playList").addClass("hidden")
-    // $(".player_listing").addClass("hidden")
-    // $('.player_form').removeClass('hidden');
+  $('.onShowPage').on('click', function (evt) {
+    var page = $(evt.currentTarget).attr('data-page');
+    $(".onPage").addClass("hidden");
+    $("." +page).removeClass("hidden");
+    // console.log("click on this button");
+
+    // // $(".playList").addClass("hidden")
+    // // $(".player_listing").addClass("hidden")
+    // // $('.player_form').removeClass('hidden');
   });
 
   // $('.showPlayer').on('click', function () {
@@ -33,7 +36,6 @@ jQuery(function ($) {
   $('.savebtn').on('click', function () {
 
     if (localStorage.getItem("playerData") === null) {
-      console.log("localStorage is empty");
 
       var id = Math.floor(Math.random() * 101);
       var name = $('.playerName').val();
@@ -44,13 +46,10 @@ jQuery(function ($) {
         "name": name,
         "url": url
       };
-      // playerData.push({"id":id, "name": name, "url": url })
-      console.log(innerObj);
       playerDataArr.push(innerObj)
 
       $('.playerName').val('');
       $('.playerUrl').val('');
-      // console.log("playerData________", playerDataArr);
       localStorage.setItem("playerData", JSON.stringify(playerDataArr));
     } else {
 
@@ -65,41 +64,54 @@ jQuery(function ($) {
         "name": name,
         "url": url
       };
-      console.log("innerObj", innerObj);
 
       playerDataArr.push(innerObj)
       for (var i = 0; i < existing.length; i++) {
-        // console.log("old data", existing[i]);
         playerDataArr.push(existing[i]);
-        // console.log("existing[i]", existing[i]);
       }
-      console.log("new data playerDataArr", innerObj.name);
-      // existing = existing ? existing.split(',') : [];
-      // var id = Math.floor(Math.random() * 101);
-      // var name = $('.playerName').val();
-      // var url = $('.playerUrl').val();
-      // var playerData_update = [];
-      // var innerObj = {
-      //   "id": id,
-      //   "name": name,
-      //   "url": url
-      // };
-      // playerData_update.push(innerObj)
-      // playerDitails.push(playerData_update);
-      // var existing = localStorage.getItem('myFavoriteSandwich');
-
-      // Otherwise, convert the localStorage string to an array
-      // existing = existing ? existing.split(',') : [];
-
-      // existing.push(playerData_update);
-
-      // localStorage.setItem('playerData', existing.toString());
-
-      // localStorage.setItem(name, existing.toString());
       $('.playerName').val('');
       $('.playerUrl').val('');
     }
     localStorage.setItem("playerData", JSON.stringify(playerDataArr));
     location.reload();
   })
+
+  var array = [{
+      name: "Chapter 1",
+      category: "Tab_1",
+      logo: "",
+      description: "Lorem poreum",
+      url: ""
+    },
+    {
+      name: "Chapter 2",
+      category: "Tab_2",
+      logo: "",
+      description: "Lorem poreum",
+      url: ""
+    }, {
+      name: "Chapter 3",
+      category: "Tab_3",
+      logo: "",
+      description: "Lorem poreum",
+      url: ""
+    }
+  ];
+  // var categories = getUnique(Array, "category");
+  // console.log("categories", categories);
+  
+  function createTab(array){
+    $(".mui-tabs__bar").html("");
+    $.each(array, function (index, playlist) {
+      $(".mui-tabs__bar").append('<li><a data-mui-toggle="tab" data-mui-controls="' +playlist.category +'">' +playlist.category +'</a></li>');
+      $(".playListPage").append('<div class="mui-tabs__pane" id="' +playlist.category +'"></div>');
+    });
+
+
+  }
+  function createList(playlist){
+      $("#" +playlist.category).append('<ul class="mui-List"> <li class="mui-item avatar"> <img src="https://homepages.cae.wisc.edu/~ece533/images/arctichare.png" alt="" class="circle"> <span class="title">chapter 2</span> <p>First Line <br> Second Line </p> </li><li class="mui-item avatar"> <img src="https://homepages.cae.wisc.edu/~ece533/images/airplane.png" alt="" class="circle"> <span class="title">chapter 1</span> <p>First Line <br> Second Line </p> </li> </ul>');
+  }
+  createTab(array);
+  createList(array[0]);
 });
